aba9be4e80d76113edeb8a390bcfe948012195f0
afellman <red.kasid@gmail.com>
2018-04-10 08:37:50 -0400

    added form submit ajax to backend

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var path = require('path')
 var express = require('express');
 var app = express();
+const bodyParser = require("body-parser");
 
 
 // require('routes')(app, path)
 app.set('view engine', 'ejs');
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 // use res.render to load up an ejs view file
 app.use(express.static(__dirname + '/'))
 
@@ -34,6 +38,11 @@ app.get('/contact', function(req, res) {
 app.get('/hummos_locater', function(req, res) {
     res.render('pages/hummos_locater');
 });
+
+
+app.post('/contact-submit', (req, res) => {
+    console.log(req.body)
+})
 app.listen(8080);
 console.log('8080 is the magic port');
 
