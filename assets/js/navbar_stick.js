0b9346540e228e2ee376831716635faa15da6fdf
afellman <red.kasid@gmail.com>
2019-02-13 20:54:18 -0500

    not much

--- assets/js/navbar_stick.js
+++ assets/js/navbar_stick.js
@@ -1,17 +1,16 @@
 
 var header = $(".navbar-collapse");
+var hero = $('#hero');
 
-
-$(document).on("scroll", function(e) {
+$(document).on("scroll", function (e) {
   // console.log($(this).scrollTop())
 
   if ($(this).scrollTop() > 190) {
     header.addClass("sticky");
-
+    hero.css('margin-top', "86px")
 
   } else {
     header.removeClass("sticky");
-
+    hero.css('margin-top', "0px")
   }
-  
 });
